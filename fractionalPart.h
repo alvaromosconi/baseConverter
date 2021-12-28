505eee257123c8e19fcf73a84bf43c16b9999992
Álvaro Mosconi <77434507+alvaromosconi@users.noreply.github.com>
2021-12-28 19:29:29 -0300

    Delete fractionalPart.h

--- fractionalPart.h
+++
@@ -1,9 +0,0 @@
-#ifndef FRACTIONALPART_H_INCLUDED
-
-#define FRACTIONALPART_H_INCLUDED
-
-float *divisionMethodFractional(char *number, int *base, int *detailed);
-
-char *multiplicationMethodFractional(char *number, int *base, int *detailed);
-
-#endif // FRACTIONALPART_H_INCLUDED
