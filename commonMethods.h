a88c654c77a22e8372a608405be8ab8a5295b3a2
Álvaro Mosconi <77434507+alvaromosconi@users.noreply.github.com>
2021-12-28 19:30:09 -0300

    Delete commonMethods.h

--- commonMethods.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMONMETHODS_H_INCLUDED
-#define COMMONMETHODS_H_INCLUDED
-
-int *transformNumberToDecimal (char *characterToTransform);
-
-int *stringLength(char *string);
-
-#endif // COMMONMETHODS_H_INCLUDED
